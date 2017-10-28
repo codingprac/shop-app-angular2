c913e247c3fc54520f580e9e8cc2577a30e6a3a4
Deep Lotia <deep.lotia@lampiris.be>
2017-10-28 21:57:40 +0200

    systemjs.config.js defined the "dist" folder to pick up the files during deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
-import { ProductComponent }  from './product.component';
+import { AppComponent }  from '/app/app.component';
+import { ProductComponent }  from '/app/product.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
